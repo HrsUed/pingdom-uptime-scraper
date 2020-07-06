48f87d94390431f5969979eb0ae2dda59335b340
HrsUed <ueda@innova-jp.com>
2020-07-06 13:55:40 +0900

    取得できなくなった不具合を修正

--- get.js
+++ get.js
@@ -1,23 +1,31 @@
-let $site, url, uptime, downtime, responsetime;
+let status, sitename, url, uptime, downtime, responsetime;
 let list = [];
-let site = {};
-let clst = [];
+let serverNo = [];
+let siteNamePrefix = /\[prod(\d)\].*/i;
+let rows = Array.from(document.getElementsByClassName("pd-table-row show"));
 
-list = [];
-site = {};
-$(".pd-table-row").each((idx, row) => {
-    $site = $($(row).children("td")[2])
-    clst = $site.children("strong")[0].innerText.match(/\[prod([1-6])\].*/i);
-    if (clst == null) {
-      clst = "corporate";
-    } else {
-      clst = clst[1];
-    }
-    url = $site.children(".pd-check-hostname")[0].innerText;
-    uptime = $(row).children("td")[5].innerText
-    $outage = $($(row).children("td")[8])
-    responsetime = $outage.children(".responsetime")[0].innerText
-    downtime = $outage.children(".js-totaldowntime")[0].innerText
-    if (responsetime != "N/A") list.push(`${clst},${url},${uptime},${downtime}`);
-});
+if ( rows.length < 1) console.log("取得できませんでした");
+
+list = rows.map((row, index) => {
+    status = row.getElementsByTagName("td")[1].getElementsByClassName("pd-check-status-up").length;
+    if (!status) return undefined;
+
+    siteName = row.getElementsByTagName("td")[2];
+
+    serverNo = siteName.getElementsByTagName("strong")[0].innerText.match(siteNamePrefix)[1];
+
+    url = siteName.getElementsByClassName("pd-check-hostname")[0].innerText;
+
+    uptime = row.getElementsByTagName("td")[5].innerText;
+    // >99.99% という表示がある場合は、>を取り除く
+    uptime = uptime.match(/^>/) ? uptime.match(/^>(.+)/)[1] : uptime;
+
+    outage = row.getElementsByTagName("td")[8];
+    responsetime = outage.getElementsByClassName("responsetime")[0].innerText;
+    downtime = outage.getElementsByClassName("js-totaldowntime")[0].innerText;
+
+    if (responsetime != "N/A") return `${serverNo},${url},${uptime},${downtime}`;
+}).filter(row => row != undefined).sort((a, b) => {
+    return parseInt(a.split(",")[0],10) - parseInt(b.split(",")[0],10);
+})
 copy(list.join("\n"));
